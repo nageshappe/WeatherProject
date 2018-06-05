954cc4d1b1a87dbe97560b4627a841cb6f88f854
Seetharam Appe <seetharama@virtusa.com>
2018-06-05 16:30:09 +0530

    Initial Weather app is created

--- js/app.js
+++ js/app.js
@@ -1,6 +1,9 @@
 $('document').ready(() => {
+    $("#chart-container").hide();
+    $('table').hide();
 $("#weatherBtn").click(() =>{
- 
+      $('table').show();
+      $("#chart-container").hide();
      const currentCity = $("#currentCity").val();
    console.log(currentCity);
      $.ajax({
@@ -20,7 +23,56 @@ $("#weatherBtn").click(() =>{
       
         }
       });
-
-
 });
+
+$("#weatherForecast").click(() => {
+    const currentCity = $("#currentCity").val();
+    $.ajax({
+        type: 'GET',
+        url: `https://api.openweathermap.org/data/2.5/forecast?q=${currentCity}&APPID=d76ce6e4f25efbe1968fa2f568de2277`,
+         success: (resp) => {
+      
+
+            plotchart(resp);
+        },
+        error: (err) => {
+      
+        }
+      });
+    });
+
+    function plotchart(resp){
+        $("table").hide();
+        $("#chart-container").show();
+        console.log(resp);
+        listofDates = resp.list.map(function(ele){return moment(ele.dt*1000).format('MMMM Do, h:mm a');});
+        listofTemps = resp.list.map(function(ele){return Math.round(ele.main.temp-270);});
+        console.log(listofTemps);
+    Highcharts.chart('chart-container', {
+        title: {
+            text: 'Weather Forecast'
+        },
+   
+    
+    
+        xAxis: {
+            categories: listofDates,
+            title :
+            {
+                text : 'Date'
+            }
+        },
+        yAxis:{
+            title:{ text : 'Temperature'}
+        },
+        series: [{
+            name: $("#currentCity").val(),
+            marker :
+            {
+                symbol : 'diamond'
+            },
+            data: listofTemps
+        }]
+        });
+}
 });
